fb6dbf64aad1af0c05a5846587caf9af22efcb8f
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2021-11-23 11:50:38 +0530

    v2

--- clone/clone/settings.py
+++ clone/clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-39=*41agfb0#l!2!)+vpdywg-x&l$db!cg9#hz(qnh6b6fmemr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
